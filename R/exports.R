4ccae9621090bf93ec5f91807faa077822986cfb
Euan Mckenzie <euan.mckenzie@kentwildlife.org.uk>
2024-07-08 15:42:18 +0100

    ReDaMoR functions are exported

---
+++ R/exports.R
@@ -0,0 +1,10 @@
+#' @importFrom ReDaMoR write_json_data_model read_json_data_model model_relational_data
+#' @export
+ReDaMoR::write_json_data_model
+
+#' @export
+ReDaMoR::read_json_data_model
+
+#' @export
+ReDaMoR::model_relational_data
+
