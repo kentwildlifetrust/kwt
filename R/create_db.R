68c43fd30ccb869c1fedc234199f3fa4ddb003dd
kialuna <77881537+kialuna@users.noreply.github.com>
2024-07-12 14:24:46 +0100

    Added create database and user role

--- R/create_db.R
+++ R/create_db.R
@@ -1,16 +1,21 @@
-# create_db <- function(conn = db, db_name = "my_db"){
-#
-#   #get script from SQL folder in package directory
-#   script <- system.file("SQL/create_db.sql", package = "kwt") %>%
-#     readLines() %>%
-#     paste(collapse = "\n")
-#
-#   vals <- list(
-#     admin_user = Sys.getenv("admin_user"),
-#     admin_password = Sys.getenv("admin_password"),
-#     db_name = db_name
-#   )
-#
-#   query <- glue::glue_data_sql(vals, script, .con = conn)
-#   dbExecute(conn, query)
-# }
+create_db <- function(conn, db_name, app_role){
+  # Create database based on temnplate
+  DBI::dbExecute(conn, glue::glue_sql("CREATE DATABASE {DBI::dbQuoteIdentifier(conn,db_name)} TEMPLATE kwt_database_template;", .con=conn))
+  # Write the app_role to the logins table and fetch the password
+  password<-DBI::dbGetQuery(conn, glue::glue_sql("WITH updated_row AS (
+                                                  UPDATE admin.logins
+                                                  SET username = {DBI::dbQuoteString(conn,app_role)}
+                                                  WHERE kwtid = (
+                                                    SELECT MIN(kwtid)
+                                                    FROM admin.logins
+                                                    WHERE (username IS NULL OR username = '')
+                                                  )
+                                                  RETURNING password
+                                                )
+                                                SELECT * FROM updated_row;", .con=conn))
+  # Create the user with app_role and fetched password
+  DBI::dbExecute(conn, glue::glue_sql("CREATE USER {DBI::dbQuoteIdentifier(conn,app_role)} WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT NOREPLICATION CONNECTION LIMIT -1 PASSWORD {password};", .con=conn))
+  # Print to console
+  print(glue::glue("Database {db_name} created and user {app_role} created with password {password}"))
+}
+
