ef5d946c2e06e990c76753885bb4bd51aaa9f86b
kialuna <77881537+kialuna@users.noreply.github.com>
2024-07-16 11:48:12 +0100

    create_db - set search path to include postgis

--- R/create_db.R
+++ R/create_db.R
@@ -30,6 +30,9 @@ create_db <- function(conn, db_name, app_role){
                                                 SELECT * FROM updated_row;", .con=conn))
   # Create the user with app_role and fetched password
   DBI::dbExecute(conn, glue::glue_sql("CREATE USER {DBI::dbQuoteIdentifier(conn,app_role)} WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT NOREPLICATION CONNECTION LIMIT -1 PASSWORD {password};", .con=conn))
+
+  # Set search path to include postgis schema
+  DBI::dbExecute(conn, glue::glue_sql("ALTER DATABASE {DBI::dbQuoteIdentifier(conn,db_name)} SET search_path TO postgis, public;", .con=conn))
   # Print to console
   print(glue::glue("Database {db_name} created and user {app_role} created with password {password}"))
 }
